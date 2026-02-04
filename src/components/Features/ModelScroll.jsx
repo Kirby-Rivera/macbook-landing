5703f9ea6b01c64129c10b61421aae63dcba2ae5
Kirby Rivera <kirbyrivera@Kirbys-Mac-mini.local>
2026-02-04 08:46:15 +0800

    Refactor some things

--- src/components/Features/ModelScroll.jsx
+++ src/components/Features/ModelScroll.jsx
@@ -49,14 +49,14 @@ const ModelScroll = ({ featureSequence }) => {
 
     if (groupRef.current) {
       modelTimeline.to(groupRef.current.rotation, {
-        y: Math.PI * 2,
+        x: Math.PI * 0.1,
         ease: "power1.inOut",
       });
     }
 
     timeline
       .call(() => setTexture("/videos/feature-1.mp4"))
-      .to(".box1", { opacity: 1, y: 0, delay: 1 })
+      .to(".box1", { opacity: 1, y: 0, delay: 0.5 })
 
       .call(() => setTexture("/videos/feature-2.mp4"))
       .to(".box2", { opacity: 1, y: 0 })
